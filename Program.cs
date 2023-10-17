915ee291ee17de5c2a190a64c5210d25bfee7156
Eric Comanescu <ericcom@outlook.com>
2023-10-17 18:34:17 -0400

    part 2 done and very dumb way lmao

--- Program.cs
+++ Program.cs
@@ -33,18 +33,62 @@ class Program
             {'y', 24},
             {'z', 25},
         };
+
+        Dictionary<int, char> indexChar = new()
+        {
+            {0,'a'},
+            {1,'b'},
+            {2,'c'},
+            {3,'d'},
+            {4,'e'},
+            {5,'f'},
+            {6,'g'},
+            {7,'h'},
+            {8,'i'},
+            {9,'j'},
+            {10,'k'},
+            {11,'l'},
+            {12,'m'},
+            {13,'n'},
+            {14,'o'},
+            {15,'p'},
+            {16,'q'},
+            {17,'r'},
+            {18,'s'},
+            {19,'t'},
+            {20,'u'},
+            {21,'v'},
+            {22,'w'},
+            {23,'x'},
+            {24,'y'},
+            {25,'z'},
+        };
         string[] data = File.ReadAllLines(@".\input");
 
         bool[] present = new bool[26];
 
         int result = 0;
-        string line = "";
+        List<string> lines = new();
         for (int i = 0; i < data.Length; i++)
         {
             for (int j = 0; j < data[i].Length; j++)
             {
-                if (!present[charIndex[data[i][j]]])
-                    present[charIndex[data[i][j]]] = true;
+                if ((i != 0 && data[i - 1] == "") || i == 0)
+                {
+                    if (!present[charIndex[data[i][j]]])
+                        present[charIndex[data[i][j]]] = true;
+                }
+                else
+                {
+                    for (int k = 0; k < present.Length; k++)
+                    {
+                        if (present[k])
+                        {
+                            if (!data[i].Contains(indexChar[k]))
+                                present[k] = false;
+                        }
+                    }
+                }
             }
 
             if (data[i] == "" || i == data.Length - 1)
@@ -53,7 +97,7 @@ class Program
                 {
                     if (present[j])
                         result += 1;
-                    
+
                     present[j] = false;
                 }
             }
